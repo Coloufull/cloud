d5a25a8a8acc34cfa7876b542cd21d17af9eddea
Coloufull <15181401624@163.com>
2021-08-18 20:33:37 +0800

    添加

---
+++ configclient/src/main/java/com/cloud/controller/ClientHandler.java
@@ -0,0 +1,20 @@
+package com.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class ClientHandler {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/index")
+    public String index(){
+        return this.port;
+    }
+
+}
