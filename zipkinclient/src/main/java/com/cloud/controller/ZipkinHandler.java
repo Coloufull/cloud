7705f83bf7b036eccc712c66811088cda9c5e667
Coloufull <15181401624@163.com>
2021-08-19 19:39:36 +0800

    更新

--- zipkinclient/src/main/java/com/cloud/controller/ZipkinHandler.java
+++ zipkinclient/src/main/java/com/cloud/controller/ZipkinHandler.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/client")
-public class ClientHandler {
+@RequestMapping("/zipkin")
+public class ZipkinHandler {
 
     @Value("${server.port}")
-    private  String port;
+    private String port;
 
     @GetMapping("/index")
-    public  String  index(){
-        return  this.port;
+    public String index(){
+        return this.port;
     }
 }
